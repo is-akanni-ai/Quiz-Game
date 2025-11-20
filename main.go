ea1604c9de9c4d6d7c19af74e2544258ad77e6dd
Abdullahi <ashleyalvins010@gmail.com>
2025-11-20 14:20:30 +0100

    end of quiz

--- main.go
+++ main.go
@@ -24,7 +24,8 @@ func main() {
 
 
 	score := 0
-	numOfQst := 2
+	numOfQst := 4
+	qstAnswered := 0
 
 	fmt.Print("Qst 1: What is better?, RTX 3080 or RTX 3090 ")
 
@@ -36,16 +37,16 @@ func main() {
 	switch answe1 + " " + answe2 {
         case "RTX 3090":
 		    fmt.Println("Correct!")
-			score += 5
-			// numOfQst += 1
+			score ++
+			qstAnswered += 1
 			
 	    case "rtx 3090":
 			fmt.Println("Correct!")
-			score += 5
-			// numOfQst += 1
+			score ++
+			qstAnswered += 1
 	default:
 		fmt.Println("Incorrect!")
-		// numOfQst += 1
+		qstAnswered += 1
 	}
 
 	fmt.Print("Qst 2: How many cores does the AMD Ryzen 9 3900X have? ")
@@ -55,19 +56,45 @@ func main() {
 
 	if cores == 12 {
 		fmt.Println("Correct!")
-		score += 5
-		// numOfQst += 1
+		score ++
+		qstAnswered += 1
 	}else{
 		fmt.Println("Incorrect!")
-		// numOfQst += 1
+		qstAnswered += 1
 	}
 
-	fmt.Println("Qst 3: ")
+	fmt.Print("Qst 3: What does CPU stand for? ")
 
-	fmt.Printf("You score %v out of 20 with %v.\n questions", score, numOfQst);
+	var cpuAns string;
+	fmt.Scan(&cpuAns);
+
+	if cpuAns == "Central Processing Unit" || cpuAns == "central processing unit" {
+		fmt.Println("Correct!")
+		score ++
+		qstAnswered += 1
+	}else{
+		fmt.Println("Incorrect!")
+		qstAnswered += 1
+	}
+
+	fmt.Print("Qst 4: What is the capital of France? ");
+
+	var capital string;
+	fmt.Scan(&capital);
+
+    if capital == "Paris" || capital == "paris" {
+		fmt.Println("Correct!")
+		score++
+		qstAnswered += 1
+	} else {
+		fmt.Println("Incorrect!")
+		qstAnswered += 1
+	}
+
+	fmt.Printf("Congrats %v, you have completed the quiz!\n", name)
+	fmt.Printf("You anwered %v correctly out of %v questions, total questions answered  %v. questions \n", score, numOfQst, qstAnswered);
 
 	percent := (float64(score) / float64(numOfQst)) * 100
 
-
-	fmt.Printf("You scored:  %v%%", percent);
+	fmt.Printf("You scored:  %.0f%%\n", percent)
 }
